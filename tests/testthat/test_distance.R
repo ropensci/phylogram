7822bc2ac32dff6e0350f5f7b715f83e823b15ab
shaunpwilkinson <shaunpwilkinson@gmail.com>
2017-06-09 17:21:27 +0900

    Added tests for amino acid alignments

--- tests/testthat/test_distance.R
+++ tests/testthat/test_distance.R
@@ -13,17 +13,27 @@ rawbases <- as.raw(c(136, 40, 72, 24))
 xDNA <- lapply(x, function(s) rawbases[match(s, bases)])
 class(xDNA) <- "DNAbin"
 
-# simulate an AA sequence dataset
+# simulate an AA sequence dataset this time an alignment
 set.seed(999)
 aminos <- LETTERS[-c(2, 10, 15, 21, 24, 26)]
-y <- list(sample(aminos, replace = TRUE, size = 100))
+y <- matrix(sample(aminos, replace = TRUE, size = 100), nrow = 1)
 evolve <- function(a) if(runif(1) > 0.95) sample(aminos, 1) else a
-for(i in 2:10) y[[i]] <- unname(sapply(y[[i - 1]], evolve))
-names(y) <- paste("Sequence", 1:10)
+for(i in 2:10) y <- rbind(y, sapply(y[i - 1,], evolve))
+rownames(y) <- paste("Sequence", 1:10)
 # convert to AAbin object
 rawaminos <- as.raw((65:89)[-c(2, 10, 15, 21, 24, 26)])
-yAA <- lapply(y, function(s) rawaminos[match(s, aminos)])
+yAA <- apply(y, c(1, 2), function(s) rawaminos[match(s, aminos)])
 class(yAA) <- "AAbin"
+
+# aminos <- LETTERS[-c(2, 10, 15, 21, 24, 26)]
+# y <- list(sample(aminos, replace = TRUE, size = 100))
+# evolve <- function(a) if(runif(1) > 0.95) sample(aminos, 1) else a
+# for(i in 2:10) y[[i]] <- unname(sapply(y[[i - 1]], evolve))
+# names(y) <- paste("Sequence", 1:10)
+# # convert to AAbin object
+# rawaminos <- as.raw((65:89)[-c(2, 10, 15, 21, 24, 26)])
+# yAA <- lapply(y, function(s) rawaminos[match(s, aminos)])
+# class(yAA) <- "AAbin"
 
 # count k-mers
 x.kcounts <- kcount(x)
